e7d3a59e3d53251f90c65da30dc666d2c2d6821e
jyootai <jyootg@gmail.com>
2015-01-29 15:25:02 +0800

    Don't allow no keyword search

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,6 +5,11 @@ class HomesController < ApplicationController
 
   def search
     @query = params[:note_search][:query]
+    if !@query.blank?
       redirect_to user_path(@query)
+    else
+      redirect_to root_path
+      #TODO flash message
+    end
   end
 end
