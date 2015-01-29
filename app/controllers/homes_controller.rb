dd862c3023341f261e07843a1ac1aebf6b5c74b3
jyootai <jyootg@gmail.com>
2015-01-29 15:04:15 +0800

    Implement username search function

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,9 @@ class HomesController < ApplicationController
   def index
     @note = Note.new
   end
+
+  def search
+    @query = params[:note_search][:query]
+      redirect_to user_path(@query)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#index'
+  get '/search'  =>'homes#search', :as=>'search'
   get ':username/:note_id'  =>"notes#show", :as=>"note"
   get ':username'  =>"notes#user", :as=>"user"
   resources :notes, except: [:show]
