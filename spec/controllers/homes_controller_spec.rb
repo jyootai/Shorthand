2134aa941fee96da40523bcbaaa6ff6bb7c1ad32
jyootai <jyootg@gmail.com>
2015-02-01 20:44:00 +0800

    modify header content

--- spec/controllers/homes_controller_spec.rb
+++ spec/controllers/homes_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe HomesController, :type => :controller do
+  let(:note){FactoryGirl.create(:note)}
 
   describe "GET index" do
     it "returns http success" do
@@ -9,4 +10,10 @@ RSpec.describe HomesController, :type => :controller do
     end
   end
 
+  #describe "GET search" do
+    #it "returns http success" do
+      #get :search
+      #expect(response).to have_http_status(:success)
+    #end
+  #end
 end
---
+++ spec/factories/notes.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :note do
+    time_at "2015-01-22 19:40:00"
+    body "Hello Test"
+    username "test1"
+    note_id "1"
+  end
+end
