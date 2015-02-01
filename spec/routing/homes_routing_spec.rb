acabfd689e33df0402f4855b006786cc0294c412
jyootai <jyootg@gmail.com>
2015-02-01 20:45:40 +0800

    delect unnecessary file

--- spec/routing/homes_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-RSpec.describe HomesController, :type=>:routing do
-  describe "homes routing" do
-    it "routes to / " do
-      expect(:get=>"/").to route_to(:controller=>"homes",
-                                    :action=>"index")
-    end
-  end
-end
